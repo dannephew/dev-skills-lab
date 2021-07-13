85689d8cd402fdbdb0155aef37b08ad68a8490b0
dannephew <danneszhang@gmail.com>
2021-07-13 16:15:51 -0700

    load host

---
+++ data/skill-db.js
@@ -0,0 +1,7 @@
+
+const skills = [
+    {text: 'Eating', valueable: true, _id: 125223},
+    {text: 'Sleeping', valuable: true, _id: 127904},
+    {text: 'Dreaming', valuable: true, _id: 139608},
+  ]
+  
